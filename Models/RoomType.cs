a6e3db0de8a7339a6f2b9c511bc8a8c5d76b5da7
dgzdgz <dgrr4br@gmail.com>
2020-10-27 20:54:09 -0400

    added RoomType class and modified existing code to use it

---
+++ Models/RoomType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJJDHotel.Models
+{
+    public class RoomType
+    {
+        //should room type have a location? weird to have room type with ocean view in Kansas City
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Beds { get; set; }
+        public string View { get; set; }
+        public string RoomName { get; set; }
+        public decimal Rate { get; set; }
+        public string ImgPath { get; set; }
+    }
+}
