10bcc8181075f0291a3c93c32cd4ca7934322b94
JustinFlames12 <Justin.Dea3@gmail.com>
2020-11-13 18:44:43 -0500

    added SQLite Database

---
+++ Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJJDHotel.Models
+{
+    public class Room
+    {
+        public int RoomNumber { get; set; }
+        public int RoomId { get; set; }
+        public RoomType RoomType { get; set; }
+        public int RoomTypeId { get; set; }
+    }
+}
