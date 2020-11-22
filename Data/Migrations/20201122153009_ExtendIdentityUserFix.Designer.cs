0ae3d17be966160839035e5a6374e0e82a466c89
dgzdgz <dgrr4br@gmail.com>
2020-11-22 10:55:07 -0500

    Fix IdentityUser ApplicationUser connection, add migration, create five reservations

--- Data/Migrations/20201122153009_ExtendIdentityUserFix.Designer.cs
+++ Data/Migrations/20201122153009_ExtendIdentityUserFix.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using AJJDHotel.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AJJDHotel.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201122153009_ExtendIdentityUserFix")]
+    partial class ExtendIdentityUserFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
