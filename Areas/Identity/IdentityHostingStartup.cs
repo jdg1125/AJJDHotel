9a50cb558b0f0d1ffd4794c5fda04f8640fd9aec
Akele White <36961533+akele15@users.noreply.github.com>
2020-11-22 09:25:07 -0800

    made the registration page visible

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using AJJDHotel.Data;
+using AJJDHotel.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AJJDHotel.Areas.Identity.IdentityHostingStartup))]
+namespace AJJDHotel.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
