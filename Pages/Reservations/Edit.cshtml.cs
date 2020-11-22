250b37932f1c35a3e5c2e21a9493456ff8388f9d
dgzdgz <dgrr4br@gmail.com>
2020-11-22 10:24:31 -0500

    Add Reservations CRUD, change IdentityUser to ApplicationUser

---
+++ Pages/Reservations/Edit.cshtml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AJJDHotel.Data;
+using AJJDHotel.Models;
+
+namespace AJJDHotel.Pages.Reservations
+{
+    public class EditModel : PageModel
+    {
+        private readonly AJJDHotel.Data.ApplicationDbContext _context;
+
+        public EditModel(AJJDHotel.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Reservation Reservation { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Reservation = await _context.Reservations
+                .Include(r => r.ApplicationUser)
+                .Include(r => r.Room).FirstOrDefaultAsync(m => m.ReservationId == id);
+
+            if (Reservation == null)
+            {
+                return NotFound();
+            }
+           ViewData["Id"] = new SelectList(_context.ApplicationUsers, "Id", "Id");
+           ViewData["RoomId"] = new SelectList(_context.Rooms, "RoomId", "RoomId");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Reservation).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReservationExists(Reservation.ReservationId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ReservationExists(int id)
+        {
+            return _context.Reservations.Any(e => e.ReservationId == id);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using AJJDHotel.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AJJDHotel.Models;
 
 namespace AJJDHotel
 {
@@ -30,7 +31,7 @@ namespace AJJDHotel
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
         }
