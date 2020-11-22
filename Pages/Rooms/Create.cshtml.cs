b4fea04354a346aecb7ffeb1b604a54111cffd13
dgzdgz <dgrr4br@gmail.com>
2020-11-21 19:36:55 -0500

    Fill Rooms table, modified all 5 Rooms CRUD pages to use RoomTypeId instead of Beds

---
+++ Pages/Rooms/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AJJDHotel.Data;
+using AJJDHotel.Models;
+
+namespace AJJDHotel.Pages.Rooms
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AJJDHotel.Data.ApplicationDbContext _context;
+
+        public CreateModel(AJJDHotel.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["RoomTypeId"] = new SelectList(_context.RoomTypes, "RoomTypeId", "RoomTypeId");
+            return Page();
+        }
+
+        [BindProperty]
+        public Room Room { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Rooms.Add(Room);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Rooms/Delete.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AJJDHotel.Data;
+using AJJDHotel.Models;
+
+namespace AJJDHotel.Pages.Rooms
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly AJJDHotel.Data.ApplicationDbContext _context;
+
+        public DeleteModel(AJJDHotel.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Room Room { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Room = await _context.Rooms
+                .Include(r => r.RoomType).FirstOrDefaultAsync(m => m.RoomId == id);
+
+            if (Room == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Room = await _context.Rooms.FindAsync(id);
+
+            if (Room != null)
+            {
+                _context.Rooms.Remove(Room);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
