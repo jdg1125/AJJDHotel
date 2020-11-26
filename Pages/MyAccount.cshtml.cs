f0c5151c667975e6cb8513b4ae29f71dbb1289d2
Akele White <36961533+akele15@users.noreply.github.com>
2020-11-26 13:42:39 -0800

    made method to pass reservation id and gutted my account page

--- Pages/MyAccount.cshtml.cs
+++ Pages/MyAccount.cshtml.cs
@@ -18,6 +18,8 @@ namespace AJJDHotel.Pages
         public ApplicationUser ApplicationUser { get; set; }
         public List<Reservation> UsersReservations { get; set; }
 
+        public int num { get; set; }
+
 
 
         public MyAccountModel(IDbAccess dbAccess, UserManager<ApplicationUser> userManager)
@@ -32,6 +34,11 @@ namespace AJJDHotel.Pages
             string id = _userManager.GetUserId(User);
             ApplicationUser = dbAccess.GetUserById(id);
             UsersReservations = dbAccess.GetReservationsByUserId(id);
+        }
+        public void OnGetCancel(int id)
+        {
+            num = id;
+             this.OnGet();
         }
 
         public int MakeConfirmationNumber(int pk)
